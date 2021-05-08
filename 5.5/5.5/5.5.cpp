cf848e814d228481ac7b8dc54a57ee7cf3866f85
Thrackerzood <71223383+Thrackerzood@users.noreply.github.com>
2021-05-08 17:10:14 +0400

    Add files via upload

---
+++ 5.5/5.5/5.5.cpp
@@ -0,0 +1,17 @@
+﻿#include  <stdio.h>
+
+int main(void)
+{
+
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; ++i) {
+        if (n % i == 0) printf(i == n ? "%d" : "%d ", i);
+    }
+    printf("\n");
+    for (int i = n; i > 0; --i) {
+        if (n % i == 0) printf(i == 1 ? "%d" : "%d ", i);
+    }
+    return 0;
+}
